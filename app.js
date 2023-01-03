9522d555c03d76ce671de6ab807c0f57e5b9527e
Leonardo Dias <106618445+Leodias2204@users.noreply.github.com>
2023-01-03 20:48:33 -0300

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const somaFunc = require ("./somar")
-const subFunc = require("./sub")
-const multFunc = require("./mult")
-const divFunc = require("./div")
-
-
-console.log(`O valor da conta é ${multFunc(9586,95).toFixed(2)}`)
